48f7ff2b56eeabd975db7549b2db009e3f31a951
kanekotic <me@kanekotic.com>
2022-08-12 09:18:55 +0200

    Alvaro | added local notification support

--- App.tsx
+++ App.tsx
@@ -2,14 +2,34 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import * as BackgroundFetch from "expo-background-fetch"
 import * as TaskManager from "expo-task-manager"
+import * as Notifications from 'expo-notifications'
 
 const TASK_NAME = "BACKGROUND_TASK"
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: true,
+  }),
+});
 
 TaskManager.defineTask(TASK_NAME, () => {
   try {
     // fetch data here...
     const receivedNewData = "Simulated fetch " + (Math.random()*1000)
     console.log("My task ", receivedNewData)
+    Notifications.scheduleNotificationAsync(
+        {
+          content:{
+            title: 'Barklarm',
+            body: `My task ${receivedNewData}`
+          },
+          trigger: {
+            seconds: 1
+          }
+        }
+    );
     return receivedNewData
       ? BackgroundFetch.BackgroundFetchResult.NewData
       : BackgroundFetch.BackgroundFetchResult.NoData
@@ -21,7 +41,7 @@ TaskManager.defineTask(TASK_NAME, () => {
 const RegisterBackgroundTask = async () => {
   try {
     await BackgroundFetch.registerTaskAsync(TASK_NAME, {
-      minimumInterval: 300, // seconds,
+      minimumInterval: 5, // seconds,
       startOnBoot: true,
       stopOnTerminate: false,
     })
