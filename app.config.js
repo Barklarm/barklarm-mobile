544d655ca294b4ffe2b94b93e9ef307d50d546a1
kanekotic <me@kanekotic.com>
2022-08-24 10:56:09 +0200

    Alvaro | enable update of versions

--- app.config.js
+++ app.config.js
@@ -20,14 +20,16 @@ export default {
   ],
   "ios": {
     "supportsTablet": true,
-    "bundleIdentifier": "com.kanekotic.barklarm"
+    "bundleIdentifier": "com.kanekotic.barklarm",
+    "buildNumber": project.version
   },
   "android": {
     "adaptiveIcon": {
       "foregroundImage": "./assets/adaptive-icon.png",
       "backgroundColor": "#FFFFFF"
     },
-    "package": "com.kanekotic.barklarm"
+    "package": "com.kanekotic.barklarm",
+    "versionCode": project.version.split(".").map(number =>Number(number)).reduce((total, value, index) => total + (value * [10000,100,1][index]) , 0)
   },
   "web": {
     "favicon": "./assets/favicon.png"
