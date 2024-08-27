fb98c25221548e41770ecb5a27cfa435056187c8
alvaro lorente <contact@alvarolorente.dev>
2024-08-27 11:52:28 +0200

    feat: link to expo project

--- app.config.js
+++ app.config.js
@@ -41,8 +41,14 @@ export default {
   experiments: {
     typedRoutes: true,
   },
-  "extra": {
-    "eas": {
+  updates: {
+    url: "https://u.expo.dev/6002db10-a1bc-4c3a-9332-f42d17dbd6a2"
+  },
+  runtimeVersion: {
+    policy: "appVersion"
+  },
+  extra: {
+    eas: {
       "projectId": "6002db10-a1bc-4c3a-9332-f42d17dbd6a2"
     }
   }
